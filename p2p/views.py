e6b217214911aa5d01c4eadf88e81fb54106a8d0
vovota-company <vovotacompany@gmail.com>
2026-06-26 10:51:13 -0400

    add p2p order

--- p2p/views.py
+++ p2p/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema_view, extend_schema
@@ -9,42 +9,43 @@ from .serializers import *
 
 class CustomResponseMixin:
     def success(self, data=None, message="Success", status_code=status.HTTP_200_OK):
-        return Response({
-            "success": True,
-            "message": message,
-            "data": data
-        }, status=status_code)
+        return Response(
+            {
+                "success": True,
+                "message": message,
+                "data": data,
+            },
+            status=status_code,
+        )
 
     def error(self, message="Error", status_code=status.HTTP_400_BAD_REQUEST):
-        return Response({
-            "success": False,
-            "message": message,
-            "data": None
-        }, status=status_code)
+        return Response(
+            {
+                "success": False,
+                "message": message,
+                "data": None,
+            },
+            status=status_code,
+        )
 
 
 # -------------------------
 # PAYMENT METHOD
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=PaymentMethodSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=PaymentMethodSerializer,
-        responses=PaymentMethodSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class PaymentMethodViewSet(CustomResponseMixin, viewsets.ViewSet):
+class PaymentMethodViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
 
     def list(self, request):
-        qs = PaymentMethod.objects.all()
-        return self.success(PaymentMethodSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = PaymentMethodSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return self.success(serializer.data, "Created", status.HTTP_201_CREATED)
@@ -54,32 +55,29 @@ class PaymentMethodViewSet(CustomResponseMixin, viewsets.ViewSet):
 # MY PAYMENT METHOD
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=MyPaymentMethodSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=MyPaymentMethodSerializer,
-        responses=MyPaymentMethodSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class MyPaymentMethodViewSet(CustomResponseMixin, viewsets.ViewSet):
+class MyPaymentMethodViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    serializer_class = MyPaymentMethodSerializer
     permission_classes = [IsAuthenticated]
+    queryset = MyPaymentMethod.objects.all()
+
+    def get_queryset(self):
+        return MyPaymentMethod.objects.filter(user=self.request.user).order_by("-created_at")
 
     def list(self, request):
-        qs = MyPaymentMethod.objects.filter(user=request.user).order_by("-created_at")
-        return self.success(MyPaymentMethodSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = MyPaymentMethodSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
-
         return self.success(
             serializer.data,
             "Payment method added",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
 
 
@@ -87,24 +85,19 @@ class MyPaymentMethodViewSet(CustomResponseMixin, viewsets.ViewSet):
 # CURRENCY
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=CurrencySerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=CurrencySerializer,
-        responses=CurrencySerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class CurrencyViewSet(CustomResponseMixin, viewsets.ViewSet):
+class CurrencyViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
 
     def list(self, request):
-        qs = Currency.objects.all()
-        return self.success(CurrencySerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = CurrencySerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return self.success(serializer.data, "Created", status.HTTP_201_CREATED)
@@ -114,32 +107,26 @@ class CurrencyViewSet(CustomResponseMixin, viewsets.ViewSet):
 # SELL OFFER
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=SellOfferSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=SellOfferSerializer,
-        responses=SellOfferSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class SellOfferViewSet(CustomResponseMixin, viewsets.ViewSet):
+class SellOfferViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = SellOffer.objects.all().order_by("-created_at")
+    serializer_class = SellOfferSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        qs = SellOffer.objects.all().order_by("-created_at")
-        return self.success(SellOfferSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = SellOfferSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
-
         return self.success(
             serializer.data,
             "Sell offer created",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
 
 
@@ -147,32 +134,26 @@ class SellOfferViewSet(CustomResponseMixin, viewsets.ViewSet):
 # BUY OFFER
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=BuyOfferSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=BuyOfferSerializer,
-        responses=BuyOfferSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class BuyOfferViewSet(CustomResponseMixin, viewsets.ViewSet):
+class BuyOfferViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = BuyOffer.objects.all().order_by("-created_at")
+    serializer_class = BuyOfferSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        qs = BuyOffer.objects.all().order_by("-created_at")
-        return self.success(BuyOfferSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = BuyOfferSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
-
         return self.success(
             serializer.data,
             "Buy offer created",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
 
 
@@ -180,32 +161,26 @@ class BuyOfferViewSet(CustomResponseMixin, viewsets.ViewSet):
 # SELL ORDER
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=SellOrderSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=SellOrderSerializer,
-        responses=SellOrderSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class SellOrderViewSet(CustomResponseMixin, viewsets.ViewSet):
+class SellOrderViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = SellOrder.objects.all().order_by("-created_at")
+    serializer_class = SellOrderSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        qs = SellOrder.objects.all().order_by("-created_at")
-        return self.success(SellOrderSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = SellOrderSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return self.success(
             serializer.data,
             "Sell order created",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
 
 
@@ -213,32 +188,26 @@ class SellOrderViewSet(CustomResponseMixin, viewsets.ViewSet):
 # BUY ORDER
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=BuyOrderSerializer(many=True),
-        tags=["P2P"]
-    ),
-    create=extend_schema(
-        request=BuyOrderSerializer,
-        responses=BuyOrderSerializer,
-        tags=["P2P"]
-    )
+    list=extend_schema(tags=["P2P"]),
+    create=extend_schema(tags=["P2P"]),
 )
-class BuyOrderViewSet(CustomResponseMixin, viewsets.ViewSet):
+class BuyOrderViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = BuyOrder.objects.all().order_by("-created_at")
+    serializer_class = BuyOrderSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        qs = BuyOrder.objects.all().order_by("-created_at")
-        return self.success(BuyOrderSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = BuyOrderSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return self.success(
             serializer.data,
             "Buy order created",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
 
 
@@ -246,30 +215,24 @@ class BuyOrderViewSet(CustomResponseMixin, viewsets.ViewSet):
 # COIN
 # -------------------------
 @extend_schema_view(
-    list=extend_schema(
-        responses=CoinSerializer(many=True),
-        tags=["Wallet"]
-    ),
-    create=extend_schema(
-        request=CoinSerializer,
-        responses=CoinSerializer,
-        tags=["Wallet"]
-    )
+    list=extend_schema(tags=["Wallet"]),
+    create=extend_schema(tags=["Wallet"]),
 )
-class CoinViewSet(CustomResponseMixin, viewsets.ViewSet):
+class CoinViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    queryset = Coin.objects.all().order_by("id")
+    serializer_class = CoinSerializer
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        qs = Coin.objects.all().order_by("id")
-        return self.success(CoinSerializer(qs, many=True).data)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return self.success(serializer.data)
 
     def create(self, request):
-        serializer = CoinSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return self.success(
             serializer.data,
             "Coin created",
-            status.HTTP_201_CREATED
+            status.HTTP_201_CREATED,
         )
