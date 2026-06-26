fec2e79a54a14182aa489ccaaf30b36acf48ca26
vovota-company <vovotacompany@gmail.com>
2026-06-26 09:50:11 -0400

    add p2p order

--- p2p/admin.py
+++ p2p/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import * 
+from .models import *
 
 
 admin.site.register(PaymentMethod)
+admin.site.register(MyPaymentMethod)
 admin.site.register(Currency)
+
 admin.site.register(SellOffer)
 admin.site.register(BuyOffer)
+
+admin.site.register(SellOrder)
+admin.site.register(BuyOrder)
